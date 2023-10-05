29d061db2219d9a1398c813aeff17974a9a5bd7a
lunaro-4 <pavelike@gmail.com>
2023-10-05 10:55:27 +0300

    Function refactor for ease of unification; empty days not appended now

--- commit_concatenate/views.py
+++ commit_concatenate/views.py
@@ -1,14 +1,29 @@
 from django.shortcuts import render
 from .github_parcer import parse
+import datetime
 
+def concat_to_weekdays(data_input):
+    sorted(data_input.items(), key=lambda x: x[1])
+    data  = {
+        0:[],
+        1:[],
+        2:[],
+        3:[],
+        4:[],
+        5:[],
+        6:[],
+    }
+    for i in data_input.keys():
+        weekday = datetime.datetime.fromtimestamp(i).weekday()
+        data[weekday].append([i,data_input[i]])
+    return data
 
 def form_table(request):
     name = 'xddd'
-    
-    data = parse() 
-
+    data = parse()
+    data = concat_to_weekdays(data)
     context = {
             'name':name,
-            'data':data
+            'data':data,
     }
     return render(request=request, template_name='commit_concatenate/index.html', context=context)
